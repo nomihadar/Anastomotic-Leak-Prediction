336849a305da863f6cf654fbae03fe0718659cae
nomihadar <hadar.nomi@gmail.com>
2020-05-20 13:41:00 +0300

    upload

---
+++ Scripts/utils/constants.py
@@ -0,0 +1,12 @@
+
+#name of Y column 
+Y_COL = 'Anastomotic Leak'
+#index of Y column
+Y_COL_IDX = 0 
+
+#row number(s) to use as the column names
+MATRIX_HEADER = [0, 1, 2, 3]
+
+
+#
+WRITE_FLAG = False
