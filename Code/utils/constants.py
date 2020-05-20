b3128aba3c7039efb6fac56a6a9aa8e9c03d0d17
nomihadar <hadar.nomi@gmail.com>
2020-05-20 17:05:41 +0300

    unite files

--- Code/utils/constants.py
+++ Code/utils/constants.py
@@ -1,3 +1,27 @@
+import os, sys
+# solution to my problem I had: didn't recognize conda
+# https://github.com/numpy/numpy/issues/15183
+
+
+'''
+Definitions 
+'''
+
+ROOT_PATH = os.path.dirname(os.path.dirname(sys.path[0])) #"C:/Users/hp4mp/PythonProjects/Anastomotic-Leak-Prediction/"
+
+#data paths
+DATA_PATH = os.path.join(ROOT_PATH, "Data")
+DPATH_SOURCES = os.path.join(DATA_PATH, "sourceFiles")  
+DPATH_EVENTS = os.path.join(DATA_PATH, "events")  
+DPATH_MATRICES =  os.path.join(DATA_PATH, "matrices")  
+DPATH_EDA = os.path.join(DATA_PATH, "EDA") #Exploratory Data Analysis 
+
+
+DICT_PATH = ROOT_PATH + "MetaData/Hebrew_English_Dictionary/"
+
+'''
+Constants
+'''
 
 #name of Y column 
 Y_COL = 'Anastomotic Leak'
