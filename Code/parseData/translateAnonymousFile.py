d4c382fb9f43d40be824a19514498e243c785a5a
nomihadar <hadar.nomi@gmail.com>
2020-06-18 13:56:58 +0300

    add file to translate

--- Code/parseData/translateAnonymousFile.py
+++ Code/parseData/translateAnonymousFile.py
@@ -7,16 +7,25 @@ from utils.defs_functions import getHebrewWords
 import argparse 
 import pandas as pd
 
+
+#files names 
+INPUT_FILE_1 = "anonymous.xlsx"
+INPUT_FILE_2 = "anonymousFull.xlsx"
+
 DICT_FILE = "dictionary_anonymous_file.xlsx"
 
-def createDictionary(df, dict_path):
+OUT_FILE_1 = "anonymous_translated.xlsx"
+OUT_FILE_2 = "anonymousFull_translated.xlsx"
+
+def createDictionary(df1, df2, dict_path):
     #extract the Hebrew words, and create a dictionary file
     # (add translations manually)
 
     #get the Hebrew words in the data frame
-    df_hebrew_cells = getHebrewWords(df) 
-    df_hebrew_cols = pd.DataFrame({"words" : list(df.columns[1:11])})
-    df_hebrew = pd.concat([df_hebrew_cells, df_hebrew_cols])
+    df1_cells = getHebrewWords(df1) 
+    df1_cols = getHebrewWords(list(df1.columns))
+    df2_cols = getHebrewWords(list(df2.columns))
+    df_hebrew = pd.concat([df1_cells, df1_cols, df2_cols])
 
     df_hebrew.to_excel(dict_path, index=False, header=False) #uncomment after creating dictionary
 
@@ -27,47 +36,46 @@ def translate(df, dict_path):
     dictionary = pd.read_excel(dict_path, header=None, index_col=0).to_dict()[1]
     dictionary
 
-    #translate 
+    #translate cells
     df.replace(dictionary, inplace=True)
 
     #translate headers
-    headers = df.columns
-    new_headers = [dictionary[header] 
-                    if header in dictionary 
-                    else header.title() 
-                    for header in headers]
-    df.columns = new_headers
+    headers = pd.Series(df.columns).replace(dictionary)
+    df.columns = pd.Index(headers.str.title())
 
 
 if __name__ == "__main__":
     
-    #get names of files
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-file', type=str, help='input name of anonymous file')
-    parser.add_argument('-out', type=str, help='output name of translated-anonymous file')
-    args = parser.parse_args()
+    #read input files
+    input_path = os.path.join(DPATH_SOURCES, INPUT_FILE_1) 
+    df1 = pd.read_excel(input_path)
+
+    input_path = os.path.join(DPATH_SOURCES, INPUT_FILE_2) 
+    df2 = pd.read_excel(input_path)
     
-    #concatenate paths
-    input_path = os.path.join(DPATH_ANONYMOUS, args.file)
-    output_path = os.path.join(DPATH_ANONYMOUS, args.out)
+    print("files sizes:", df1.shape, df2.shape)
+    print(df1.head)
+    print(df2.head)
 
-    #read anonymous tabel 
-    df = pd.read_excel(input_path)
-
-    print("file size:", df.shape)
-    print(df.head)
+    #dict path
+    dict_path = os.path.join(DPATH_ANONYMOUS, DICT_FILE) 
 
     #if dictionary does not exist yet (first execute) - create one
-    dict_path = os.path.join(DPATH_ANONYMOUS, DICT_FILE) 
     if not os.path.exists(dict_path):
-        createDictionary(df, dict_path)
+        createDictionary(df1, df2, dict_path)
 
     #translate 
-    translate(df, dict_path)
+    translate(df1, dict_path)
+    translate(df2, dict_path)
 
-    #save translated file 
+    #save translated files
+    output_path = os.path.join(DPATH_ANONYMOUS, OUT_FILE_1) 
     with pd.ExcelWriter(output_path, datetime_format='DD/MM/YYYY') as writer:
-        df.to_excel(writer, index =False)
+        df1.to_excel(writer, index =False)
+
+    output_path = os.path.join(DPATH_ANONYMOUS, OUT_FILE_2) 
+    with pd.ExcelWriter(output_path, datetime_format='DD/MM/YYYY') as writer:
+        df2.to_excel(writer, index =False)
 
 
 
